e56d2efd2c322cca07c4d1be3b05e5df2b3ea9f1
zuoban <1061669148@qq.com>
2026-05-15 00:20:06 +0800

    Initial Commit

---
+++ ClipTyper/ClipTyperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipTyperApp.swift
+//  ClipTyper
+//
+//  Created by 王锦强 on 2026/5/15.
+//
+
+import SwiftUI
+
+@main
+struct ClipTyperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipTyper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipTyper
+//
+//  Created by 王锦强 on 2026/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
